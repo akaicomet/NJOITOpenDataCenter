367e695711c805bf884823fba2267d807b626cb7
akaicomet <45959235+akaicomet@users.noreply.github.com>
2018-12-19 02:31:42 -0500

    Update YourMoneyRetiredPensionMembers.py

--- YourMoneyRetiredPensionMembers.py
+++ YourMoneyRetiredPensionMembers.py
@@ -3,7 +3,7 @@ def RetiredPensionMembersDataWrangler(_data):
     #replace 'NA'
     RetiredPension_df['member_middle_initial'].fillna('')
 
-    RetiredPension_df_grped = RetiredPension_df[['report_year_month','member_frst_name','member_middle_initial','member_last_name','member_birth_year','member_last_employer','member_pension_fund','member_retirement_date','retirement_cancellation_date','ytd_total_paymnts','member_salary_for_calculation']]
+    RetiredPension_df_grped = RetiredPension_df[['report_year_month','member_frst_name','member_middle_initial','member_last_name','member_birth_year','member_last_employer','member_pension_fund','member_retirement_date','ytd_total_paymnts','member_salary_for_calculation']]
     RetiredPension_df_grped= RetiredPension_df_grped.sort_values(['member_last_name','member_frst_name','member_middle_initial','member_birth_year','member_retirement_date','report_year_month']) 
     RetiredPension_df_grped['ytd_total_paymnts'] = RetiredPension_df_grped['ytd_total_paymnts'].astype('float64')
     
